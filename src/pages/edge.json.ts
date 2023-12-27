0fb605eb699a3635ef921c14e9f206f4f9af294e
Jordi <jllahi@gmail.com>
2023-12-27 23:30:18 +0100

    Update json endpoint

---
+++ src/pages/edge.json.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from 'astro';
+
+export async function GET(): APIRoute {
+	return new Response(JSON.stringify({ time: new Date() }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'Cache-Control': 's-maxage=10, stale-while-revalidate',
+		},
+	});
+}
