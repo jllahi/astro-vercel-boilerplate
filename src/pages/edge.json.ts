bc59cf05276e2e702789d6845ad06b69fe4de027
Jordi <jllahi@gmail.com>
2023-12-27 23:36:37 +0100

    Update code

--- src/pages/edge.json.ts
+++ src/pages/edge.json.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro';
 
-export async function GET(): APIRoute {
+export async function GET({ params, request }): APIRoute {
 	return new Response(JSON.stringify({ time: new Date() }), {
 		status: 200,
 		headers: {
