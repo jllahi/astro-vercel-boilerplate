0b79fb8839faa9fb013797d6fd4921096a05859c
Jordi <jllahi@gmail.com>
2023-12-27 23:21:40 +0100

    Update Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 import { defineConfig } from 'astro/config'
-// Use Vercel Edge Functions (Recommended)
-import vercel from '@astrojs/vercel/edge'
-// Can also use Serverless Functions
-// import vercel from '@astrojs/vercel/serverless';
-// Or a completely static build
-// import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
 	output: 'server',
