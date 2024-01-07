7d9b9703e5d9dbb1129f4900042408c82c05b120
Jordi <jllahi@gmail.com>
2024-01-07 12:16:56 +0100

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,13 @@ import vercel from '@astrojs/vercel/serverless'
 export default defineConfig({
 	output: 'server',
 	adapter: vercel({
+		devImageService: 'squoosh',
 		imageService: true,
+		imagesConfig: {
+			sizes: [320, 640, 1280],
+		},
+		webAnalytics: {
+			enabled: true,
+		},
 	}),
 })
