11287d4c8362570aae55b59f42bfbc08f7baec81
jllahi <1439075+jllahi@users.noreply.github.com>
2024-01-31 12:33:40 +0100

    Update image config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,17 @@ export default defineConfig({
 	adapter: vercel({
 		devImageService: 'squoosh',
 		imageService: true,
-		// imagesConfig: {
-		// 	sizes: [320, 640, 1280],
-		// },
+		imagesConfig: {
+			sizes: [320, 640, 1280],
+			domains: [],
+			remotePatterns: [
+				{
+					// protocol: 'https',
+					// hostname: '^via\\.placeholder\\.com$',
+					// pathname: '^/1280x640/.*$',
+				},
+			],
+		},
 		webAnalytics: {
 			enabled: true,
 		},
