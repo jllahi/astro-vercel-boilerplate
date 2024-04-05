fd7e4875e846d5d7e90a5ffe5d1ee02ad8701c81
jllahi <1439075+jllahi@users.noreply.github.com>
2024-04-05 17:37:23 +0200

    Update edge endpoint

--- src/pages/edge.json.ts
+++ src/pages/edge.json.ts
@@ -1,39 +1,31 @@
 import type { APIRoute } from 'astro'
-// import type { APIContext } from 'astro'
+import type { APIContext } from 'astro'
 
 export const prerender = false
 
-// export function GET({ params, request }): APIRoute {
-export function GET({}): APIRoute {
-	return (context) => {
-		return new Response(JSON.stringify({ time: new Date() }), {
+// context.params from APIContext
+// export const GET: APIRoute = ({ params, request }: APIContext) => {
+export const GET: APIRoute = (context: APIContext) => {
+	return new Response(
+		JSON.stringify({
+			time: new Date(),
+			clientAddress: context.clientAddress,
+			site: context.site,
+			url: context.url,
+			generator: context.generator,
+			userAgent: context.request.headers.get('user-agent'),
+			// request: context.request,
+			// params: context.params,
+			// headers: context.request.headers,
+			// cookies: context.cookies,
+			// locals: context.locals,
+		}),
+		{
 			status: 200,
 			headers: {
 				'Content-Type': 'application/json',
 				'Cache-Control': 's-maxage=10, stale-while-revalidate',
 			},
-		})
-	}
+		}
+	)
 }
-
-// import type { APIRoute } from 'astro'
-
-// export const prerender = false
-
-// export const GET: APIRoute = ({ params, request }) => {
-// 	return new Response(
-// 		JSON.stringify({
-// 			path: new URL(request.url).pathname,
-// 		})
-// 	)
-// }
-
-// import type { APIContext } from 'astro'
-
-// // export function GET({ request }: APIContext) {
-// export async function GET(context: APIContext) {
-// 	return {
-// 		// body: `Hola ${request.url}`,
-// 		body: `Hola ${context.site}`,
-// 	}
-// }
