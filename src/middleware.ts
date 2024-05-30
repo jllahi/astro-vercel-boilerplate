c41511a9936e6c44e1a50276ae406a6e79f86473
jllahi <1439075+jllahi@users.noreply.github.com>
2024-05-30 14:44:20 +0200

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+// This helper automatically types middleware params
+import { defineMiddleware } from 'astro:middleware'
+
+export const onRequest = defineMiddleware((context, next) => {
+  // intercept data from a request
+  // optionally, modify the properties in `locals`
+  context.locals.title = 'New Title'
+  context.locals.property = 'New Property'
+
+  // return a Response or the result of calling `next()`
+  return next()
+})
