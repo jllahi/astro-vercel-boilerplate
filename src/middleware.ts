044307c2d02e76205df266b526579dcc76d33647
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-24 18:37:09 +0200

    disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ import { defineMiddleware } from 'astro:middleware'
 export const onRequest = defineMiddleware((context, next) => {
   // intercept data from a request
   // optionally, modify the properties in `locals`
-  context.locals.title = 'New Title'
-  context.locals.property = 'New Property'
+  // context.locals.title = 'New Title'
+  // context.locals.property = 'New Property'
 
   // return a Response or the result of calling `next()`
   return next()
